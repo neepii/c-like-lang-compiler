7b4842ecebd08f4f9f052e59e64dea214545e100
samodurov.nikita <nikita98351@gmail.com>
2025-07-12 22:42:27 +0300

    feat(code gen): implement return statement

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
 (* -*- compile-command: "opam exec -- dune exec compiler"; -*- *)
 
 open Compiler.Parse
+open Compiler.Gen
 
 let () =
   let ic = open_in Sys.argv.(1) in
@@ -8,7 +9,14 @@ let () =
     let line = In_channel.input_all ic in
     let tokens = tokenize_text line in
     let ast = parse tokens in
+    let dag = create_dag ast in
+    let output = generate_code dag in
+    let oc = open_out "/tmp/X0101011.s" in
+    output_string oc output;
+    let _ = Unix.open_process_in "riscv64-linux-gnu-as -march=rv64gc /tmp/X0101011.s -o /tmp/temp.o && riscv64-linux-gnu-ld /tmp/temp.o" in
+    print_endline output;
     print_ast ast;
+    close_in ic;
   with e ->
     close_in_noerr ic;
     raise e
--- lib/parse.ml
+++ lib/parse.ml
@@ -8,7 +8,7 @@ type token_type = IntLiteral
                 | RightParenthesis
                 | LeftCurly
                 | RightCurly
-                | Keyword
+                | ReturnKeyword
                 | IfKeyword
                 | ElseKeyword
                 | WhileKeyword
@@ -45,6 +45,7 @@ type expr = Variable of string
 type stmt = Assignment of ident * expr
           | IfStatement of expr * stmt list * stmt list
           | WhileStatement of expr * stmt list
+          | ReturnStatement of expr
           | EndStatement
 
 type program = stmt list
@@ -56,6 +57,7 @@ let token_type_key_value = [
     "if", IfKeyword;
     "while", WhileKeyword;
     "else", ElseKeyword;
+    "return", ReturnKeyword;
     "=", AssignmentSymbol;
     "[a-zA-Z][a-zA-z0-9]*", Identifier;
     "[+-]", FirstOperator;
@@ -68,18 +70,18 @@ let token_type_key_value = [
     "}", RightCurly;
 ]
 
-let token_type_regexp = 
+let token_type_regexp =
   List.map (fun pair -> (Str.regexp (fst pair) , (snd pair))) token_type_key_value
 
 let rec tokenize_text text_string =
-  if text_string = "" then 
+  if text_string = "" then
     [("", EOF)]
-  else 
+  else
     let matched_regexp = List.find (fun regexp -> Str.string_match (fst regexp) text_string 0) token_type_regexp in
     let end_index = Str.match_end () in
     let token_string = String.sub text_string 0 end_index in
     let new_string = String.sub text_string end_index ((String.length text_string) - end_index) in
-    if snd matched_regexp == Whitespace then 
+    if snd matched_regexp == Whitespace then
       tokenize_text new_string
     else
       let token = (token_string, snd matched_regexp) in
@@ -98,7 +100,7 @@ let parse_match (ttype: token_type) token_list =
     | [] -> raise (Failure "Can't match anything with empty list")
     | h :: t -> if snd h = ttype then t else raise (Failure ("Syntax error: match error on token " ^ (fst h)))
 
-let get_rel str = 
+let get_rel str =
   match str with
   | ">" -> Bigger
   | "<" -> Less
@@ -106,13 +108,13 @@ let get_rel str =
   | "<=" -> LessEqual
   | _ -> raise (Failure "Can't parse relational operator")
 
-let get_equ str = 
+let get_equ str =
   match str with
   | "==" -> Equal
   | "!=" -> NotEqual
   | _ -> raise (Failure "Can't parse equality operator")
 
-let get_sign str = 
+let get_sign str =
   match str with
   | "+" -> Add
   | "-" -> Sub
@@ -121,19 +123,19 @@ let get_sign str =
   | _ -> raise (Failure "Can't parse arithmetic operator")
 
 (* i need to refactor this *)
-let get_string_rel str = 
+let get_string_rel str =
   match str with
-  | Bigger      -> ">"   
-  | Less        -> "<"   
-  | BiggerEqual -> ">=" 
-  | LessEqual   -> "<=" 
+  | Bigger      -> ">"
+  | Less        -> "<"
+  | BiggerEqual -> ">="
+  | LessEqual   -> "<="
 
-let get_string_equ str = 
+let get_string_equ str =
   match str with
-  |  Equal    -> "==" 
-  |  NotEqual -> "!=" 
+  |  Equal    -> "=="
+  |  NotEqual -> "!="
 
-let get_string_sign oper = 
+let get_string_sign oper =
   match oper with
   | Add -> "+"
   | Sub -> "-"
@@ -142,10 +144,10 @@ let get_string_sign oper =
 
 
 let rec parse_sum token_list =
-  match token_list with 
+  match token_list with
     | [] -> raise (Failure "Illegal state in parse_sum")
     | x :: [] -> (fst (parse_prod [x])), []
-    | x -> 
+    | x ->
           let product, rem = parse_prod x in
           if rem = [] then (product, rem)
           else if snd (List.hd rem) = FirstOperator then  (
@@ -155,19 +157,19 @@ let rec parse_sum token_list =
           else
             (product, rem)
 
-and parse_prod token_list = 
+and parse_prod token_list =
   match token_list with
     | [] -> raise (Failure "Illegal state in parse_prod")
-    | x :: [] -> 
+    | x :: [] ->
        (parse_const x, [])
-    | h :: t -> 
+    | h :: t ->
        match snd h with
          (* refactor me *)
        | LeftParenthesis ->
           let expression, rem = parse_sum t in
           let rem_without_rightparenthesis = parse_match RightParenthesis rem in
           if rem_without_rightparenthesis = [] then (expression, rem_without_rightparenthesis)
-          else if snd (List.hd rem_without_rightparenthesis) = SecondOperator then 
+          else if snd (List.hd rem_without_rightparenthesis) = SecondOperator then
             let operator = get_sign (fst (List.hd rem_without_rightparenthesis)) in
             let product, rem = parse_prod (List.tl rem_without_rightparenthesis) in
             (Bop (expression, operator, product), rem)
@@ -180,7 +182,7 @@ and parse_prod token_list =
             let loc_operator = get_sign (fst operator) in
             let loc_product, rem = parse_prod (List.tl t) in
             (Bop (constant, loc_operator, loc_product)) , rem)
-          else 
+          else
             (constant, t)
        | Identifier ->
           let variable = parse_variable h in
@@ -189,9 +191,9 @@ and parse_prod token_list =
             let loc_operator = get_sign (fst operator) in
             let loc_product, rem = parse_prod (List.tl t) in
             (Bop (variable, loc_operator, loc_product)) , rem)
-          else 
+          else
             (variable, t)
-       | _ -> 
+       | _ ->
           failwith "unimpl case in parse_prod"
 
 let rec parse_expr token_list =
@@ -212,13 +214,17 @@ let rec parse_expr token_list =
           else
             (product, rem)
 
-let rec parse_stmt (token_list: (string * token_type) list) = 
+let rec parse_stmt (token_list: (string * token_type) list) =
   let stmt, tail =
     match token_list with
     | [] -> raise (Failure "No statements ahead")
     | h :: tail ->
-       match snd h with 
-       | Identifier -> 
+       match snd h with
+       | ReturnKeyword ->
+          let expression, tail = parse_expr tail in
+          let tail = parse_match Punctuator tail in
+          (ReturnStatement expression, tail)
+       | Identifier ->
           let tail = parse_match AssignmentSymbol tail in
           let expression, tail = parse_expr tail in
           let tail = parse_match Punctuator tail in
@@ -238,7 +244,7 @@ let rec parse_stmt (token_list: (string * token_type) list) =
           let tail = parse_match LeftCurly tail in
           let first_stmts, tail = parse_stmt tail in
           let tail = parse_match RightCurly tail in
-          if tail = [] || snd (List.hd tail) != ElseKeyword then 
+          if tail = [] || snd (List.hd tail) != ElseKeyword then
             (IfStatement (expression, first_stmts, []), tail)
           else
             let tail = parse_match ElseKeyword tail in
@@ -248,11 +254,11 @@ let rec parse_stmt (token_list: (string * token_type) list) =
             (IfStatement (expression, first_stmts, second_stmts), tail)
        | EOF ->
           (EndStatement, [])
-       | _ -> failwith "unimplemented case in parse_stmt" 
+       | _ -> failwith "unimplemented case in parse_stmt"
   in
   if tail = [] || snd (List.hd tail) = RightCurly then
     ([stmt], tail)
-  else 
+  else
     let stmts, tail = parse_stmt tail in
     (stmt :: stmts, tail)
 
@@ -263,21 +269,21 @@ let parse (token_list: (string * token_type) list) =
 
 let rec print_expr ast =
   match ast with
-  | Relation (x, y, z) -> 
+  | Relation (x, y, z) ->
      print_string "(";
      print_expr x;
      let sign = get_string_rel y in
      print_string sign;
      print_expr z;
      print_string ")"
-  | Equality (x, y, z) -> 
+  | Equality (x, y, z) ->
      print_string "(";
      print_expr x;
      let sign = get_string_equ y in
      print_string sign;
      print_expr z;
      print_string ")"
-  | Bop (x, y, z) -> 
+  | Bop (x, y, z) ->
      print_string "(";
      print_expr x;
      let sign = get_string_sign y in
@@ -289,7 +295,11 @@ let rec print_expr ast =
 
 let rec print_stmt ast_list =
   List.iter (fun ast ->
-      match ast with 
+      match ast with
+      | ReturnStatement x ->
+         print_string "return ";
+         print_expr x;
+         print_string " ; ";
       | Assignment (x, y) ->
          print_string ("(" ^ x ^ ") = ");
          print_expr y;
